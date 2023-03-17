198e65da17cc14e2e76d8d57159983d9e148c51b
Samuel José Candido <101359102+SamuelCandido@users.noreply.github.com>
2023-03-17 13:54:58 +0000

    Começando a criar parte do front para ter os primeiros testes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs')
 
 //rota padrão
 app.get("/", function(req, res){
     res.send("Está é a rota padrão, link do repositorio deste projeto: https://github.com/SamuelCandido/url_shortener");
 });
 
+app.get("/home", function(req, res){
+    res.render('index')
+})
 
 
 
@@ -13,7 +18,7 @@ app.get("/", function(req, res){
 
 
 
-app.listen(8080, function(){
-    console.log("Servidor operante na url http://localhost:8081")
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Servidor operante na porta 5000")
 });
 //localhost para criar a aplicação web
