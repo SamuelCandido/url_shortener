274291b3325b39df6591b2f41df8316c32bb41fc
SamuelCandido <samueljosecandido@gmail.com>
2023-03-20 14:18:05 -0300

    Teste

--- server.js
+++ server.js
@@ -12,21 +12,17 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 
 //Rotas:
-//Rota padrão
-app.get("/", function(req, res){
-    res.send("Está é a rota padrão, link do repositório deste projeto: https://github.com/SamuelCandido/url_shortener");
-})
 
 //Rota home page
-app.get("/home", async function (req, res) {
+app.get("/", async function (req, res) {
         const shortUrls = await ShortUrl.find()
         res.render('index', { shortUrls: shortUrls });
-    })
+})
 
 //Sincronizando, esperar concluir em segundo plano para continuar
 app.post('/shortUrls', async function(req, res){
     await ShortUrl.create({ full: req.body.fullUrl})
-    res.redirect('/home')
+    res.redirect('/')
 })
 
 app.get('/:shortUrl', async function(req, res){
@@ -37,6 +33,8 @@ app.get('/:shortUrl', async function(req, res){
       
     res.redirect(shortUrl.full)
 })
+
+router
 
 app.listen(process.env.PORT || 5000, function(){
     console.log("Servidor operante na porta 5000")
