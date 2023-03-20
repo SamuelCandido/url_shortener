65ab59593ef5e2e2c3d5a42a0b79fa08150eb5f1
SamuelCandido <samueljosecandido@gmail.com>
2023-03-20 17:59:03 -0300

    Tentativa 6

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose =  require('mongoose');
 const ShortUrl = require('./models/shortUrl');
-const connection = require('./config/connection')
+const connection = require('./config/connection');
 const app = express();
 
-
+if ($_server['REQUEST_URI']=='/favicon.ico') exit('');
 //
 app.set('view engine', 'ejs')
 
@@ -21,7 +21,7 @@ app.get("/", function(req, res){
 app.get("/home", async function (req, res) {
         const shortUrls = await ShortUrl.find()
         res.render('index', { shortUrls: shortUrls });
-    })
+})
 
 //Sincronizando, esperar concluir em segundo plano para continuar
 app.post('/shortUrls', async function(req, res){
